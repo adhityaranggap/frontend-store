aa03a40d9f07ec635ff0769dc85611e978da6361
cloudsample6 <cloudsample6@gmail.com>
2020-12-09 10:57:18 +0700

    edit logo store

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,103 +1,117 @@
 <template>
-    <header class="section-header">
-        <section class="header-main border-bottom">
-            <div class="container-fluid">
-                <div class="row align-items-center">
-                    <div class="col-md-3 col-7">
-                        <router-link :to="{name: 'home'}" class="text-decoration-none" data-abc="true">
-                            <span class="logo"><i class="fa fa-apple-alt"></i> APPLE STORE </span>
-                        </router-link>
-                    </div>
-                    <div class="col-md-5 d-none d-md-block">
-                        <form class="search-wrap">
-                            <div class="input-group w-100"><input type="text" class="form-control search-form"
-                                    style="width:55%;border: 1px solid #ffffff" name="q"
-                                    placeholder="mau beli apa hari ini ?">
-                                <div class="input-group-append">
-                                    <button class="btn search-button" type="submit"><i class="fa fa-search"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-md-4 col-5">
-                        <div class="d-flex justify-content-end">
-
-                            <div class="cart-header">
-                                  <router-link :to="{name: 'cart'}" class="btn search-button btn-md" style="color: #ffffff;background-color: #6677ef;border-color: #ffffff;"><i class="fa fa-shopping-cart"></i> {{ cartCount }} | Rp. {{ moneyFormat(cartTotal) }} </router-link>
-                            </div>
-
-                            <div class="account">
-                                <router-link :to="{name: 'login'}" v-if="!isLoggedIn" class="btn search-button btn-md d-none d-md-block ml-4"><i class="fa fa-user-circle"></i> ACCOUNT</router-link>
-                                <router-link :to="{name: 'dashboard'}" v-else class="btn search-button btn-md d-none d-md-block ml-4"><i class="fa fa-tachometer-alt"></i> DASHBOARD </router-link>      
-                            </div>
-
-                        </div>
-                    </div>
+  <header class="section-header">
+    <section class="header-main border-bottom">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-md-3 col-7">
+            <router-link
+              :to="{ name: 'home' }"
+              class="text-decoration-none"
+              data-abc="true"
+            >
+              <span class="logo"><i class="fa fa-apple-alt"></i> INTERNET STORE </span>
+            </router-link>
+          </div>
+          <div class="col-md-5 d-none d-md-block">
+            <form class="search-wrap">
+              <div class="input-group w-100">
+                <input
+                  type="text"
+                  class="form-control search-form"
+                  style="width: 55%; border: 1px solid #ffffff"
+                  name="q"
+                  placeholder="mau beli apa hari ini ?"
+                />
+                <div class="input-group-append">
+                  <button class="btn search-button" type="submit">
+                    <i class="fa fa-search"></i>
+                  </button>
                 </div>
+              </div>
+            </form>
+          </div>
+          <div class="col-md-4 col-5">
+            <div class="d-flex justify-content-end">
+              <div class="cart-header">
+                <router-link
+                  :to="{ name: 'cart' }"
+                  class="btn search-button btn-md"
+                  style="color: #ffffff; background-color: #6677ef; border-color: #ffffff"
+                  ><i class="fa fa-shopping-cart"></i> {{ cartCount }} | Rp.
+                  {{ moneyFormat(cartTotal) }}
+                </router-link>
+              </div>
+
+              <div class="account">
+                <router-link
+                  :to="{ name: 'login' }"
+                  v-if="!isLoggedIn"
+                  class="btn search-button btn-md d-none d-md-block ml-4"
+                  ><i class="fa fa-user-circle"></i> ACCOUNT</router-link
+                >
+                <router-link
+                  :to="{ name: 'dashboard' }"
+                  v-else
+                  class="btn search-button btn-md d-none d-md-block ml-4"
+                  ><i class="fa fa-tachometer-alt"></i> DASHBOARD
+                </router-link>
+              </div>
             </div>
-        </section>
-    </header>
+          </div>
+        </div>
+      </div>
+    </section>
+  </header>
 </template>
 
 <script>
-import { computed, onMounted } from 'vue'
-import { useStore } from 'vuex'
-    export default {
+import { computed, onMounted } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    //store vuex
+    const store = useStore();
 
-        setup() {
+    //computed
+    const isLoggedIn = computed(() => {
+      //get getter "isLoggedIn" dari module "auth"
+      return store.getters["auth/isLoggedIn"];
+    });
 
-            //store vuex
-            const store = useStore()
+    //cart count
+    const cartCount = computed(() => {
+      //get getter "cartCount" dari module "auth"
+      return store.getters["cart/cartCount"];
+    });
 
-            //computed
-            const isLoggedIn = computed(() => {
+    //cart total
+    const cartTotal = computed(() => {
+      //get getter "cartTotal" dari module "auth"
+      return store.getters["cart/cartTotal"];
+    });
 
-                //get getter "isLoggedIn" dari module "auth"
-                return store.getters['auth/isLoggedIn']
+    //mounted
+    onMounted(() => {
+      //check state token
+      const token = store.state.auth.token;
 
-            })
+      if (!token) {
+        return;
+      }
 
-            //cart count
-            const cartCount = computed(() => {
+      //saat mounted, akan memanggil action "cartCount" di module "cart"
+      store.dispatch("cart/cartCount");
 
-                //get getter "cartCount" dari module "auth"
-                return store.getters['cart/cartCount']
-            })
+      //saat mounted, akan memanggil action "cartTotal" di module "cart"
+      store.dispatch("cart/cartTotal");
+    });
 
-            //cart total
-            const cartTotal = computed(() => {
-
-                //get getter "cartTotal" dari module "auth"
-                return store.getters['cart/cartTotal']
-            })
-
-            //mounted
-            onMounted(() => {
-
-                //check state token
-                const token = store.state.auth.token
-
-                if(!token) {
-                    return
-                }
-
-                //saat mounted, akan memanggil action "cartCount" di module "cart"
-                store.dispatch('cart/cartCount')
-
-                //saat mounted, akan memanggil action "cartTotal" di module "cart"
-                store.dispatch('cart/cartTotal')
-            })
-
-
-            return {
-                store,
-                isLoggedIn,
-                cartTotal,
-                cartCount
-            }
-
-        }
-
-    }
+    return {
+      store,
+      isLoggedIn,
+      cartTotal,
+      cartCount,
+    };
+  },
+};
 </script>
